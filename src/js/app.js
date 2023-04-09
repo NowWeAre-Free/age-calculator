d494a83cce105df8d9ef6865ccf53d68222c5e47
NowWeAre-Free <kontodarm@wp.pl>
2023-04-09 19:32:16 +0200

    add querySelectors

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+const button = document.querySelector(".btn");
+const inputDay = document.querySelector("#inputDay");
+const inputMonth = document.querySelector("#inputMonth");
+const inputYear = document.querySelector("#inputYear");
+
+const date = new Date();
+const dayNumber = date.getDate();
+const monthNumber = date.getMonth();
+const yearNumber = date.getFullYear();
+
+button.addEventListener("click", () => {});
